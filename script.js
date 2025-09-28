03a86b7163cf0d54ec4ce01b558c13baeaf7b72b
= <=>
2025-09-29 04:16:07 +0500

    v1.0.1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const calculate = () => {
   let a = parseFloat(document.getElementById("a").value) || 0;
   let b = parseFloat(document.getElementById("b").value) || 0;
 
-  let expansion = `${a}a<sup>2</sup> + ${b}b<sup>2</sup> + 2(${a}a)(${b}b)`;
+  let expansion = `(${a}a)<sup>2</sup> + (${b}b)<sup>2</sup> + 2(${a}a)(${b}b)`;
   let simplified = `${a * a}a + ${b * b}b + ${2 * a * b}ab`;
   let total = a * a + b * b + 2 * a * b;
 
