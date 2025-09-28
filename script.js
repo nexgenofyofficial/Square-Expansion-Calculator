96928192eb4efd2655b8fa5a3dde634337caa6c3
= <=>
2025-09-29 04:04:51 +0500

    Square Expansion Calculator v1.0.0

---
+++ script.js
@@ -0,0 +1,13 @@
+const calculate = () => {
+  let a = parseFloat(document.getElementById("a").value) || 0;
+  let b = parseFloat(document.getElementById("b").value) || 0;
+
+  let expansion = `${a}a<sup>2</sup> + ${b}b<sup>2</sup> + 2(${a}a)(${b}b)`;
+  let simplified = `${a * a}a + ${b * b}b + ${2 * a * b}ab`;
+  let total = a * a + b * b + 2 * a * b;
+
+  document.getElementById("result").innerHTML = `
+    <p><b>Expansion:</b> (${a}a+${b}b)<sup>2</sup> <br>  => ${expansion} <br> => ${simplified}</p>
+    <p><b>Total:</b> ${total}</p>
+  `;
+};
