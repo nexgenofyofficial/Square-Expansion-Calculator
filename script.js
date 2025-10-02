12d3e85300f50299613b1a4643cfd0fcee86f7c9
= <=>
2025-10-02 13:55:50 +0500

    simplified variable fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const calculate = () => {
   let b = parseFloat(document.getElementById("b").value) || 0;
 
   let expansion = `(${a}a)<sup>2</sup> + (${b}b)<sup>2</sup> + 2(${a}a)(${b}b)`;
-  let simplified = `${a * a}a + ${b * b}b + ${2 * a * b}ab`;
+  let simplified = `${a * a}a<sup>2</sup> + ${b * b}b<sup>2</sup> + ${2 * a * b}ab`;
   let total = a * a + b * b + 2 * a * b;
 
   document.getElementById("result").innerHTML = `
